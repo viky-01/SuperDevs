a9c1e7cb52eccaaea5a26e157a0c2414001cdfb4
viky-01 <vicky.desti.99@email.com>
2025-07-22 23:42:35 +0530

    Update main.rs with square macro example

--- week_01/src/main.rs
+++ week_01/src/main.rs
@@ -1,3 +1,11 @@
+//  Declarative macros in Rust allow you to define reusable code patterns. Here's an example of a simple macro that calculates the square of a number:
+
+macro_rules! square {
+    ($x : expr) => {
+        $x * $x
+    };
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Square is: {}" , square!(5));
 }
